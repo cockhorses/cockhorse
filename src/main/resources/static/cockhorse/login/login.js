c460d0627710d298df62af8b069cf7ab8b578629
liao <18348369382@163.com>
2020-10-09 14:00:28 +0800

    地址的取消不完善

--- src/main/resources/static/cockhorse/login/login.js
+++ src/main/resources/static/cockhorse/login/login.js
@@ -51,9 +51,24 @@ layui.use(['jquery', 'layer', 'form', 'table'], function () {
         setTimeout(function () {
             $(".text-error").html("")
         }, 5000);
-    }
-    ;
+    };
 
+    //表单输入效果
+    $(".input-item").click(function(e){
+        e.stopPropagation();
+        $(this).addClass("layui-input-focus").find(".layui-input").focus();
+    })
+    $(".layui-form-item .layui-input").focus(function(){
+        $(this).parent().addClass("layui-input-focus");
+    })
+    $(".layui-form-item .layui-input").blur(function(){
+        $(this).parent().removeClass("layui-input-focus");
+        if($(this).val() != ''){
+            $(this).parent().addClass("layui-input-active");
+        }else{
+            $(this).parent().removeClass("layui-input-active");
+        }
+    })
 });
 //启动看板娘
 initModel();
